74b76671ad7c075077000935c7dd77f29aed4ccc
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-10-15 11:14:11 +0200

    fix tuxedo driver build script

--- files/scripts/tuxedo-driver-build.sh
+++ files/scripts/tuxedo-driver-build.sh
@@ -12,6 +12,8 @@ chmod +x /usr/bin/fixtuxedo
 #And autorun
 systemctl enable /etc/systemd/system/fixtuxedo.service
 
+rpm-ostree install "kernel-devel-$(uname -r)"
+
 export HOME=/tmp
 
 cd /tmp
@@ -27,9 +29,12 @@ cd ..
 # Extract the Version value from the spec file
 export TD_VERSION=$(cat tuxedo-drivers-kmod/tuxedo-drivers-kmod-common.spec | grep -E '^Version:' | awk '{print $2}')
 
-rpm-ostree install ~/rpmbuild/RPMS/x86_64/akmod-tuxedo-drivers-$TD_VERSION-1.fc42.x86_64.rpm ~/rpmbuild/RPMS/x86_64/tuxedo-drivers-kmod-$TD_VERSION-1.fc42.x86_64.rpm ~/rpmbuild/RPMS/x86_64/tuxedo-drivers-kmod-common-$TD_VERSION-1.fc42.x86_64.rpm ~/rpmbuild/RPMS/x86_64/kmod-tuxedo-drivers-$TD_VERSION-1.fc42.x86_64.rpm
-
 KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+
+echo "Kernel version: ${KERNEL_VERSION}"
+echo "Tuxedo version: ${TD_VERSION}"
+
+rpm-ostree install ~/rpmbuild/RPMS/x86_64/akmod-tuxedo-drivers-$TD_VERSION-1.fc42.x86_64.rpm ~/rpmbuild/RPMS/x86_64/tuxedo-drivers-kmod-$TD_VERSION-1.fc42.x86_64.rpm ~/rpmbuild/RPMS/x86_64/tuxedo-drivers-kmod-common-$TD_VERSION-1.fc42.x86_64.rpm ~/rpmbuild/RPMS/x86_64/kmod-tuxedo-drivers-$TD_VERSION-1.fc42.x86_64.rpm
 
 akmods --force --kernels "${KERNEL_VERSION}" --kmod "tuxedo-drivers-kmod"
 
