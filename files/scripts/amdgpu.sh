f9105aec3e79d242f646b69fda28f71ea2ef1349
Christoph Stoettner <christoph.stoettner@stoeps.de>
2026-03-14 11:17:54 +0100

    Remove sudo from amdgpu downgrade command
    Removed 'sudo' from the downgrade command for amdgpu packages.

--- files/scripts/amdgpu.sh
+++ files/scripts/amdgpu.sh
@@ -4,7 +4,7 @@ set -oue pipefail
 echo 'This is the amdgpu firmware shell script'
 
 # Downgrade amdgpu packages
-sudo dnf -y downgrade amd-gpu-firmware-20251021-1 amd-ucode-firmware-20251021-1
+dnf -y downgrade amd-gpu-firmware-20251021-1 amd-ucode-firmware-20251021-1
 
 # Clean up repos, everything is on the image so we don't need them
 for i in $(ls /etc/yum.repos.d/ | grep -v '^fedora' | grep -v rpmfusion); do
