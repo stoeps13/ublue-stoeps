e38d0d2866bbd5dce6d66438154a4c1b25b6a924
Christoph Stoettner <christoph.stoettner@stoeps.de>
2026-03-18 12:14:20 +0100

    readd albacore, change firmware

--- files/scripts/amdgpu-el10.sh
+++ files/scripts/amdgpu-el10.sh
@@ -4,7 +4,7 @@ set -oue pipefail
 echo 'This is the amdgpu firmware shell script'
 
 # Downgrade amdgpu packages
-dnf -y downgrade amd-gpu-firmware-20251008-15.8.el10_0 amd-ucode-firmware-20251008-15.8.el10_0
+dnf -y downgrade amd-gpu-firmware-20251111-19.1.el10_1 amd-ucode-firmware-20251111-19.1.el10_1
 
 # Clean up repos, everything is on the image so we don't need them
 for i in $(ls /etc/yum.repos.d/ | grep -v '^fedora' | grep -v rpmfusion); do
