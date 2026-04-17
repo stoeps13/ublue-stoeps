d1f2a1fb4f917a7098176c16ca040bfa18cd0ac3
Christoph Stoettner <christoph.stoettner@stoeps.de>
2026-04-17 07:54:12 +0200

    downgrade firmware

--- files/scripts/amdgpu.sh
+++ files/scripts/amdgpu.sh
@@ -4,7 +4,7 @@ set -oue pipefail
 echo 'This is the amdgpu firmware shell script'
 
 # Downgrade amdgpu packages
-sudo dnf -y downgrade amd-gpu-firmware-20250613-1.fc43 amd-ucode-firmware-20250613-1.fc43
+sudo dnf -y downgrade amd-gpu-firmware-20250509-1.fc43 amd-ucode-firmware-20250509-1.fc43
 
 # Clean up repos, everything is on the image so we don't need them
 for f in /etc/yum.repos.d/*; do
