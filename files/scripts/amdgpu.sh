0b976e0ace6b22dfd2a43da7585abd9d2c57c589
Christoph Stoettner <christoph.stoettner@stoeps.de>
2026-04-18 17:24:11 +0200

    downgrade firmware to september

--- files/scripts/amdgpu.sh
+++ files/scripts/amdgpu.sh
@@ -7,11 +7,12 @@ echo 'This is the amdgpu firmware shell script'
 cd /tmp
 curl -OLk https://kojipkgs.fedoraproject.org//packages/linux-firmware/20250917/2.fc43/noarch/amd-gpu-firmware-20250917-2.fc43.noarch.rpm
 curl -OLk https://kojipkgs.fedoraproject.org//packages/linux-firmware/20250917/2.fc43/noarch/amd-ucode-firmware-20250917-2.fc43.noarch.rpm
+curl -OLk https://kojipkgs.fedoraproject.org//packages/linux-firmware/20250917/2.fc43/noarch/linux-firmware-whence-20250917-2.fc43.noarch.rpm
 # curl -OLk https://kojipkgs.fedoraproject.org/packages/linux-firmware/20250509/1.fc43/noarch/amd-ucode-firmware-20250509-1.fc43.noarch.rpm
 # curl -OLk https://kojipkgs.fedoraproject.org/packages/linux-firmware/20250509/1.fc43/noarch/amd-gpu-firmware-20250509-1.fc43.noarch.rpm
 # sudo dnf -y downgrade amd-gpu-firmware-20250509-1.fc43 amd-ucode-firmware-20250509-1.fc43
 # dnf downgrade -y /tmp/amd-ucode-firmware-20250509-1.fc43.noarch.rpm /tmp/amd-gpu-firmware-20250509-1.fc43.noarch.rpm
-dnf downgrade -y /tmp/amd-ucode-firmware-20250917-2.fc43.noarch.rpm /tmp/amd-gpu-firmware-20250917-2.fc43.noarch.rpm
+dnf downgrade -y /tmp/amd-ucode-firmware-20250917-2.fc43.noarch.rpm /tmp/amd-gpu-firmware-20250917-2.fc43.noarch.rpm /tmp/linux-firmware-whence-20250917-2.fc43.noarch.rpm
 
 # Clean up repos, everything is on the image so we don't need them
 for f in /etc/yum.repos.d/*; do
