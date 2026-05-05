c89bfc91c5200c3d7a78f7b3c2c0497f121e3e52
Christoph Stoettner <christoph.stoettner@stoeps.de>
2026-05-05 21:53:02 +0200

    update amdgpu firmware script

--- files/scripts/amdgpu.sh
+++ files/scripts/amdgpu.sh
@@ -5,25 +5,10 @@ echo 'This is the amdgpu firmware shell script'
 
 # Downgrade amdgpu packages
 cd /tmp
-curl -OLk https://kojipkgs.fedoraproject.org/packages/linux-firmware/20250917/2.fc43/noarch/amd-gpu-firmware-20250917-2.fc43.noarch.rpm
-curl -OLk https://kojipkgs.fedoraproject.org/packages/linux-firmware/20250917/2.fc43/noarch/amd-ucode-firmware-20250917-2.fc43.noarch.rpm
-curl -OLk https://kojipkgs.fedoraproject.org/packages/linux-firmware/20250917/2.fc43/noarch/linux-firmware-whence-20250917-2.fc43.noarch.rpm
-dnf downgrade -y /tmp/amd-ucode-firmware-20250917-2.fc43.noarch.rpm /tmp/amd-gpu-firmware-20250917-2.fc43.noarch.rpm /tmp/linux-firmware-whence-20250917-2.fc43.noarch.rpm
-# # curl -OLk https://kojipkgs.fedoraproject.org/packages/linux-firmware/20250509/1.fc43/noarch/amd-ucode-firmware-20250509-1.fc43.noarch.rpm
-# # curl -OLk https://kojipkgs.fedoraproject.org/packages/linux-firmware/20250509/1.fc43/noarch/amd-gpu-firmware-20250509-1.fc43.noarch.rpm
-# # sudo dnf -y downgrade amd-gpu-firmware-20250509-1.fc43 amd-ucode-firmware-20250509-1.fc43
-# # dnf downgrade -y /tmp/amd-ucode-firmware-20250509-1.fc43.noarch.rpm /tmp/amd-gpu-firmware-20250509-1.fc43.noarch.rpm
-
-# Update kernel
-echo "Starting Kernel Downgrade"
-cd /tmp
-# export KERNELVERSION=6.19.12-200.fc43
-export KERNELVERSION=6.17.8-300.fc43
-koji download-build --quiet --arch=x86_64 kernel-${KERNELVERSION} \
-  && rm ./*debug*.rpm \
-  && rm ./*uki*.rpm \
-  && dnf downgrade -y --best --allowerasing ./*${KERNELVERSION}.x86_64.rpm \
-  && rm -rf ./*.rpm
+curl -OLk https://kojipkgs.fedoraproject.org//packages/linux-firmware/20250509/1.fc43/noarch/amd-ucode-firmware-20250509-1.fc43.noarch.rpm
+curl -OLk https://kojipkgs.fedoraproject.org//packages/linux-firmware/20250509/1.fc43/noarch/amd-gpu-firmware-20250509-1.fc43.noarch.rpm
+# sudo dnf -y downgrade amd-gpu-firmware-20250509-1.fc43 amd-ucode-firmware-20250509-1.fc43
+dnf downgrade -y ./amd-gpu-firmware.*.rpm ./amd-ucode-firmware.*.rpm
 
 # Clean up repos, everything is on the image so we don't need them
 for f in /etc/yum.repos.d/*; do
